8deba35e27a63eb3299aa1b46769a9a336caaee2
Plamen Todorov <ptodorov@axway.com>
2017-02-02 15:19:02 +0200

    support for endpoints generation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const debug = require('debug')('acu-main');
 const schemaAPI = require('./lib/schema/api');
 const modelAPI = require('./lib/model/api');
+const endpointAPI = require('./lib/endpoint/api');
 const testUtils = require('./lib/util/test');
 const commonUtils = require('./lib/util/common');
 
@@ -10,8 +11,12 @@ module.exports = function (connector, options) {
 		createSchema: function (transformer, data) {
 			return schemaAPI.createSchema(connector, transformer, data);
 		},
+		//TODO Do not return models you have it in the connector
 		createModels: function (options) {
 			return modelAPI.createModels(connector, options || {});
+		},
+		createAPIs: function (endpointDescriptions) {
+			endpointAPI.createEndpoints(endpointDescriptions);
 		},
 		createModelsAndLoadFromFS: function (options) {
 			return modelAPI.createModelsAndLoadFromFS(connector, options);
@@ -21,7 +26,7 @@ module.exports = function (connector, options) {
 		},
 		getConfiguration: function () {
 			return connector.config;
-		}, 
+		},
 		common: commonUtils,
 		test: testUtils
 	};
