7ae6f46ae6203fd8e0d8d3687a9542110f56f09a
Plamen Todorov <ptodorov@axway.com>
2017-02-03 12:26:27 +0200

    introduce direct model creation (no schema step)

--- index.js
+++ index.js
@@ -8,26 +8,33 @@ const commonUtils = require('./lib/util/common');
 module.exports = function (connector, options) {
 	debug('OBTAIN API');
 	return {
-		createSchema: function (transformer, data) {
-			return schemaAPI.createSchema(connector, transformer, data);
-		},
-		//TODO Do not return models you have it in the connector
-		createModels: function (options) {
-			return modelAPI.createModels(connector, options || {});
-		},
-		createAPIs: function (endpointDescriptions) {
+		createModels: function (modelDescriptions, options) {
+			modelAPI.createFromDescription(modelDescriptions, options);
+		},		
+		createEndpoints: function (endpointDescriptions) {
 			endpointAPI.createEndpoints(endpointDescriptions);
+		},		
+		processWithSchema: {
+			createSchema: function (transformer, data) {
+				return schemaAPI.createSchema(connector, transformer, data);
+			},
+			createModelsFromSchema: function (options) {
+				return modelAPI.createModels(connector, options || {});
+			}
 		},
-		createModelsAndLoadFromFS: function (options) {
-			return modelAPI.createModelsAndLoadFromFS(connector, options);
-		},
-		getConnector: function () {
-			return connector;
-		},
-		getConfiguration: function () {
-			return connector.config;
+		metadata: {
+			getConnector: function () {
+				return connector;
+			},
+			getConfiguration: function () {
+				return connector.config;
+			}
 		},
 		common: commonUtils,
-		test: testUtils
+		test: testUtils,
+		// TODO Reconsider this one where to put it ???
+		createModelsAndLoadFromFS: function (options) {
+			return modelAPI.createModelsAndLoadFromFS(connector, options);
+		}		
 	};
 };
