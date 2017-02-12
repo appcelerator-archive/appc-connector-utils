d6fdf8b7dad62d95559dfcd555bb7e4eeec9bb05
Plamen Todorov <ptodorov@axway.com>
2017-02-13 01:10:24 +0200

    clean

--- test/server/startServer.js
+++
@@ -1,7 +0,0 @@
-const Arrow = require('arrow');
-const config = require( './config.js' );
-const serverFactory = require('./serverFactory.js')(config, addSampleModeAndAPI);
-
-function addSampleModeAndAPI(err, server) {
-	console.log("fdjshfjkdshfjdkshfkjsh");
-}
