26bddefb93a8681073ba70401e582c72c8ca99f7
Plamen Todorov <ptodorov@axway.com>
2017-03-15 07:10:30 +0200

    metod for getting the proper parent model name - namespaced and clean

--- test/integration/library.integration.js
+++ test/integration/library.integration.js
@@ -58,3 +58,42 @@ test('test validateModelMetadata - missing metadata', function (t) {
   t.notOk(result)
   t.end()
 })
+
+test('getParentModelName', function (t) {
+  const name1 = 'appc.test/myModel1'
+  const name2 = 'myModel2'
+  const name3 = 'myModel3'
+  const name4 = 'appc.test/myModel4'
+  const name5 = 'appc.test/myModel5'
+
+  const modelWithNamespace = {name: name1}
+  const modelWithoutNamespace = {name: name2}
+  const modelWithParent = {_parent: {name: name3}}
+  const modelWithParentNamespaced = {_parent: {name: name4}}
+  const modelWithParentNoName = {name: name5, _parent: {}}
+  const wrongModel = {}
+
+  const name1Result = library.utils.getParentModelName(modelWithNamespace)
+  t.equal(name1Result.nameOnly, 'myModel1')
+  t.equal(name1Result.withNamespace, name1)
+
+  const name2Result = library.utils.getParentModelName(modelWithoutNamespace)
+  t.equal(name2Result.nameOnly, name2)
+  t.equal(name2Result.withNamespace, name2)
+
+  const name3Result = library.utils.getParentModelName(modelWithParent)
+  t.equal(name3Result.nameOnly, name3)
+  t.equal(name3Result.withNamespace, name3)
+
+  const name4Result = library.utils.getParentModelName(modelWithParentNamespaced)
+  t.equal(name4Result.nameOnly, 'myModel4')
+  t.equal(name4Result.withNamespace, name4)
+
+  const name5Result = library.utils.getParentModelName(modelWithParentNoName)
+  t.equal(name5Result.nameOnly, 'myModel5')
+  t.equal(name5Result.withNamespace, name5)
+
+  t.throws(library.utils.getParentModelName.bind(null, wrongModel))
+
+  t.end()
+})
--- test/unit/arrow.unit.js
+++ test/unit/arrow.unit.js
@@ -22,6 +22,45 @@ test('createConnector', function (t) {
   t.end()
 })
 
+test('getParentModelName', function (t) {
+  const name1 = 'appc.test/myModel1'
+  const name2 = 'myModel2'
+  const name3 = 'myModel3'
+  const name4 = 'appc.test/myModel4'
+  const name5 = 'appc.test/myModel5'
+
+  const modelWithNamespace = {name: name1}
+  const modelWithoutNamespace = {name: name2}
+  const modelWithParent = {_parent: {name: name3}}
+  const modelWithParentNamespaced = {_parent: {name: name4}}
+  const modelWithParentNoName = {name: name5, _parent: {}}
+  const wrongModel = {}
+
+  const name1Result = arrowUtils.getParentModelName(modelWithNamespace)
+  t.equal(name1Result.nameOnly, 'myModel1')
+  t.equal(name1Result.withNamespace, name1)
+
+  const name2Result = arrowUtils.getParentModelName(modelWithoutNamespace)
+  t.equal(name2Result.nameOnly, name2)
+  t.equal(name2Result.withNamespace, name2)
+
+  const name3Result = arrowUtils.getParentModelName(modelWithParent)
+  t.equal(name3Result.nameOnly, name3)
+  t.equal(name3Result.withNamespace, name3)
+
+  const name4Result = arrowUtils.getParentModelName(modelWithParentNamespaced)
+  t.equal(name4Result.nameOnly, 'myModel4')
+  t.equal(name4Result.withNamespace, name4)
+
+  const name5Result = arrowUtils.getParentModelName(modelWithParentNoName)
+  t.equal(name5Result.nameOnly, 'myModel5')
+  t.equal(name5Result.withNamespace, name5)
+
+  t.throws(arrowUtils.getParentModelName.bind(null, wrongModel))
+
+  t.end()
+})
+
 // TODO Fix this test
 // test('createArrowAndStartHTTPServer', function (t) {
 //   arrowUtils.createArrowAndStartHTTPServer(config, (server) => {
